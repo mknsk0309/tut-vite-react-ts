e7a12485655a3452325472de3298d1c1424ab8bb
mknsk0309 <156224339+mknsk0309@users.noreply.github.com>
2025-02-04 08:35:06 +0000

    add : デザインの適用

--- src/App.tsx
+++ src/App.tsx
@@ -39,10 +39,12 @@ const App = () => {
   };
 
   return (
-    <div>
-      <Title />
-      <Form setCity={setCity} getWeather={getWeather} />
-      <Results results={results} />
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form setCity={setCity} getWeather={getWeather} />
+        <Results results={results} />
+      </div>
     </div>
   );
 };
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -13,14 +13,14 @@ const Results = (props: ResultsProps) => {
     <div>
       {props.results.country && (
         <div>
-          <div>{props.results.country}</div>
-          <div>{props.results.cityName}</div>
-          <div>
+          <div className="results-country">{props.results.country}</div>
+          <div className="results-city">{props.results.cityName}</div>
+          <div className="results-temp">
             {props.results.temperature}
             <span>℃</span>
           </div>
-          <div>
-            <img src={props.results.icon} />
+          <div className="results-condition">
+            <img src={props.results.icon} alt="icon" />
             <span>{props.results.conditionText}</span>
           </div>
         </div>
